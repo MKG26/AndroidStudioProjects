c370d115a4c312fe6f774bc57756c1a844b1f95f
Mohit <92150824+MKG26@users.noreply.github.com>
2023-11-21 11:24:17 +0530

    .

--- basic-android-kotlin-compose-training-lunch-tray-starter/app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ basic-android-kotlin-compose-training-lunch-tray-starter/app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -19,7 +19,12 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -66,13 +71,21 @@ fun LunchTrayAppbar(
     modifier: Modifier = Modifier
 ){
 
-    TopAppBar(
+    CenterAlignedTopAppBar(
         title = { Text(stringResource(id = currentScreen.title)) },
-        colors = TopAppBarDefaults.mediumTopAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer
-    ),
+
 
         modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
     )
 }
 @OptIn(ExperimentalMaterial3Api::class)
